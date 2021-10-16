3e615485eed2989bb67fb49d2f79dbebba29d289
niscolas <nic.cp@hotmail.com>
2021-10-16 13:10:25 -0300

    updates AtomEventUnit

--- Runtime/Events/AtomEventUnit.cs
+++ Runtime/Events/AtomEventUnit.cs
@@ -1,144 +1,168 @@
-﻿namespace Guyl.BoltAtoms.Events
+﻿using System;
+using Unity.VisualScripting;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using Void = UnityAtoms.Void;
+
+namespace Guyl.BoltAtoms.Events
 {
-	using Unity.VisualScripting;
-	using UnityAtoms;
-	using UnityAtoms.BaseAtoms;
-	using UnityEngine;
-	using System;
-	using AtomsVS.Runtime.Utility;
+    [IncludeInSettings(true)]
+    public abstract class AtomEventUnit<T, ER, E> : MachineEventUnit<EmptyEventArgs>, IAtomListener<T>
+        where E : AtomEvent<T>
+        where ER : IGetEvent, ISetEvent, new()
+    {
+        [DoNotSerializeAttribute]
+        public ValueInput _event;
 
-	[IncludeInSettings(true)]
-	public abstract class AtomEventUnit<T, V> : MachineEventUnit<EmptyEventArgs> where T : AtomEvent<V>
-	{
-		#region FIELDS
-		#region Public
-		[DoNotSerializeAttribute] public ValueInput _event;
-		[DoNotSerializeAttribute] public ValueOutput _value;
+        [DoNotSerializeAttribute]
+        public ValueOutput _value;
 
-		[DoNotSerialize, UnitHeaderInspectable, UnitButton("TriggerButton")]
-		public UnitButton _triggerButton;
-		#endregion Public
+        private Action<T> _eventRaisedHandler;
+        private T _currentValue;
 
-		#region Private
-		private EventUnit<T> _eventUnitImplementation;
-		private Action<V> _eventRaisedHandler;
-		private V _currentValue;
-		#endregion Private
-		#endregion FIELDS
+        protected override string hookName => "UnityAtomEvent";
 
-		#region PropertiesMachineEventUnit
-		protected override string hookName => "UnityAtomEvent";
-		#endregion Properties
+        protected override void Definition()
+        {
+            _event = ValueInput<ER>("", default);
+            _value = ValueOutput("", flow => _currentValue);
+            base.Definition();
+        }
 
-		#region METHODS
-		#region Protected
-		protected override void Definition()
-		{
-			_event = ValueInput<T>("", null);
-			_value = ValueOutput<V>("", flow => _currentValue);
-			base.Definition();
-		}
-		#endregion Protected
+        public override void StartListening(GraphStack stack)
+        {
+            base.StartListening(stack);
 
-		#region Public
-		public void TriggerButton(GraphReference reference)
-		{
-			if (Application.isEditor && Application.isPlaying)
-			{
-				T vEvent = Flow.FetchValue<T>(_event, reference);
-				vEvent?.Raise(vEvent.InspectorRaiseValue);
-			}
-		}
-		
-		public override void StartListening(GraphStack stack)
-		{
-			base.StartListening(stack);
+            Flow flow = Flow.New(stack.ToReference());
+            E currentEvent = flow.GetValue<ER>(_event)?.GetEvent<E>();
 
-			Flow flow = Flow.New(stack.ToReference());
-			T currentEvent = flow.GetValue<T>(_event);
+            if (!currentEvent)
+            {
+                return;
+            }
 
-			_eventRaisedHandler = (value) =>
-			{
-				flow.Invoke(trigger);
-				_currentValue = value;
-			};
+            _eventRaisedHandler = value =>
+            {
+                _currentValue = value;
+                flow.Invoke(trigger);
+            };
 
-			if (currentEvent)
-			{
-				currentEvent.Unregister(_eventRaisedHandler);
-				currentEvent.Register(_eventRaisedHandler);				
-			}
-		}
+            currentEvent.UnregisterListener(this);
+            currentEvent.RegisterListener(this, false);
+        }
 
-		public override void StopListening(GraphStack stack)
-		{
-			base.StopListening(stack);
+        public override void StopListening(GraphStack stack)
+        {
+            base.StopListening(stack);
 
-			T currentVoidEvent = Flow.FetchValue<T>(_event, stack.ToReference());
-			currentVoidEvent?.Unregister(_eventRaisedHandler);
-		}
-		#endregion Public
-		#endregion METHODS
-	}
-	
-	[UnitCategory("Events/Atoms"), UnitShortTitle("AtomBaseVariable Event"), UnitTitle("AtomBaseVariable Event")]
-	public class AtomBaseVariableEventUnit : AtomEventUnit<AtomBaseVariableEvent, AtomBaseVariable> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Bool Event"), UnitTitle("Atom Bool Event")]
-	public class BoolEventUnit : AtomEventUnit<BoolEvent, bool> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("BoolPair Event"), UnitTitle("Atom BoolPair Event")]
-	public class BoolPairEventUnit : AtomEventUnit<BoolPairEvent, BoolPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Collider2D Event"), UnitTitle("Atom Collider2D Event")]
-	public class Collider2DEventUnit : AtomEventUnit<Collider2DEvent, Collider2D> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Collider2DPair Event"), UnitTitle("Atom Collider2DPair Event")]
-	public class Collider2DPairEventUnit : AtomEventUnit<Collider2DPairEvent, Collider2DPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Collider Event"), UnitTitle("Atom Collider Event")]
-	public class ColliderEventUnit : AtomEventUnit<ColliderEvent, Collider> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("ColliderPair Event"), UnitTitle("Atom ColliderPair Event")]
-	public class ColliderPairEventUnit : AtomEventUnit<ColliderPairEvent, ColliderPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Collision2D Event"), UnitTitle("Atom Collision2D Event")]
-	public class Collision2DEventUnit : AtomEventUnit<Collision2DEvent, Collision2D> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Collision2DPair Event"), UnitTitle("Atom Collision2DPair Event")]
-	public class Collision2DPairEventUnit : AtomEventUnit<Collision2DPairEvent, Collision2DPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Collision Event"), UnitTitle("Atom Collision Event")]
-	public class CollisionEventUnit : AtomEventUnit<CollisionEvent, Collision> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("CollisionPair Event"), UnitTitle("Atom CollisionPair Event")]
-	public class CollisionPairEventUnit : AtomEventUnit<CollisionPairEvent, CollisionPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Color Event"), UnitTitle("Atom Color Event")]
-	public class ColorEventUnit : AtomEventUnit<ColorEvent, Color> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("ColorPair Event"), UnitTitle("Atom ColorPair Event")]
-	public class ColorPairEventUnit : AtomEventUnit<ColorPairEvent, ColorPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Double Event"), UnitTitle("Atom Double Event")]
-	public class DoubleEventUnit : AtomEventUnit<DoubleEvent, double> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("DoublePair Event"), UnitTitle("Atom DoublePair Event")]
-	public class DoublePairEventUnit : AtomEventUnit<DoublePairEvent, DoublePair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Float Event"), UnitTitle("Atom Float Event")]
-	public class FloatEventUnit : AtomEventUnit<FloatEvent, float> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("FloatPair Event"), UnitTitle("Atom FloatPair Event")]
-	public class FloatPairEventUnit : AtomEventUnit<FloatPairEvent, FloatPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("GameObject Event"), UnitTitle("Atom GameObject Event")]
-	public class GameObjectEventUnit : AtomEventUnit<GameObjectEvent, GameObject> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("GameObjectPair Event"), UnitTitle("Atom GameObjectPair Event")]
-	public class GameObjectPairEventUnit : AtomEventUnit<GameObjectPairEvent, GameObjectPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Int Event"), UnitTitle("Atom Int Event")]
-	public class IntEventUnit : AtomEventUnit<IntEvent, int> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("IntPair Event"), UnitTitle("Atom IntPair Event")]
-	public class IntPairEventUnit : AtomEventUnit<IntPairEvent, IntPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Quaternion Event"), UnitTitle("Atom Quaternion Event")]
-	public class QuaternionEventUnit : AtomEventUnit<QuaternionEvent, Quaternion> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("QuaternionPair Event"), UnitTitle("Atom QuaternionPair Event")]
-	public class QuaternionPairEventUnit : AtomEventUnit<QuaternionPairEvent, QuaternionPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("String Event"), UnitTitle("Atom String Event")]
-	public class StringEventUnit : AtomEventUnit<StringEvent, string> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("StringPair Event"), UnitTitle("Atom StringPair Event")]
-	public class StringPairEventUnit : AtomEventUnit<StringPairEvent, StringPair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Vector2 Event"), UnitTitle("Atom Vector2 Event")]
-	public class Vector2EventUnit : AtomEventUnit<Vector2Event, Vector2> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Vector2Pair Event"), UnitTitle("Atom Vector2Pair Event")]
-	public class Vector2PairEventUnit : AtomEventUnit<Vector2PairEvent, Vector2Pair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Vector3 Event"), UnitTitle("Atom Vector3 Event")]
-	public class Vector3EventUnit : AtomEventUnit<Vector3Event, Vector3> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Vector3Pair Event"), UnitTitle("Atom Vector3Pair Event")]
-	public class Vector3PairEventUnit : AtomEventUnit<Vector3PairEvent, Vector3Pair> {}
-	[UnitCategory("Events/Atoms"), UnitShortTitle("Void Event"), UnitTitle("Atom Void Event")]
-	public class VoidEventUnit : AtomEventUnit<VoidEvent, UnityAtoms.Void> {}
+            ER currentEventRef = Flow.FetchValue<ER>(_event, stack.ToReference());
+            E currentEvent = currentEventRef.GetEvent<E>();
+
+            if (!currentEvent)
+            {
+                return;
+            }
+
+            currentEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised(T item)
+        {
+            _eventRaisedHandler?.Invoke(item);
+        }
+    }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("AtomBaseVariable Event"), UnitTitle("AtomBaseVariable Event")]
+    public class AtomBaseVariableEventUnit : AtomEventUnit<AtomBaseVariable, AtomBaseVariableBaseEventReference, AtomBaseVariableEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Bool Event"), UnitTitle("Atom Bool Event")]
+    public class BoolEventUnit : AtomEventUnit<bool, BoolEventReference, BoolEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("BoolPair Event"), UnitTitle("Atom BoolPair Event")]
+    public class BoolPairEventUnit : AtomEventUnit<BoolPair, BoolPairEventReference, BoolPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Collider2D Event"), UnitTitle("Atom Collider2D Event")]
+    public class Collider2DEventUnit : AtomEventUnit<Collider2D, Collider2DEventReference, Collider2DEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Collider2DPair Event"), UnitTitle("Atom Collider2DPair Event")]
+    public class
+        Collider2DPairEventUnit : AtomEventUnit<Collider2DPair, Collider2DPairEventReference, Collider2DPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Collider Event"), UnitTitle("Atom Collider Event")]
+    public class ColliderEventUnit : AtomEventUnit<Collider, ColliderEventReference, ColliderEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("ColliderPair Event"), UnitTitle("Atom ColliderPair Event")]
+    public class ColliderPairEventUnit : AtomEventUnit<ColliderPair, ColliderPairEventReference, ColliderPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Collision2D Event"), UnitTitle("Atom Collision2D Event")]
+    public class Collision2DEventUnit : AtomEventUnit<Collision2D, Collision2DEventReference, Collision2DEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Collision2DPair Event"), UnitTitle("Atom Collision2DPair Event")]
+    public class Collision2DPairEventUnit : AtomEventUnit<Collision2DPair, Collision2DPairEventReference, Collision2DPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Collision Event"), UnitTitle("Atom Collision Event")]
+    public class CollisionEventUnit : AtomEventUnit<Collision, CollisionEventReference, CollisionEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("CollisionPair Event"), UnitTitle("Atom CollisionPair Event")]
+    public class CollisionPairEventUnit : AtomEventUnit<CollisionPair, CollisionPairEventReference, CollisionPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Color Event"), UnitTitle("Atom Color Event")]
+    public class ColorEventUnit : AtomEventUnit<Color, ColorEventReference, ColorEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("ColorPair Event"), UnitTitle("Atom ColorPair Event")]
+    public class ColorPairEventUnit : AtomEventUnit<ColorPair, ColorPairEventReference, ColorPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Double Event"), UnitTitle("Atom Double Event")]
+    public class DoubleEventUnit : AtomEventUnit<double, DoubleEventReference, DoubleEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("DoublePair Event"), UnitTitle("Atom DoublePair Event")]
+    public class DoublePairEventUnit : AtomEventUnit<DoublePair, DoublePairEventReference, DoublePairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Float Event"), UnitTitle("Atom Float Event")]
+    public class FloatEventUnit : AtomEventUnit<float, FloatEventReference, FloatEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("FloatPair Event"), UnitTitle("Atom FloatPair Event")]
+    public class FloatPairEventUnit : AtomEventUnit<FloatPair, FloatPairEventReference, FloatPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("GameObject Event"), UnitTitle("Atom GameObject Event")]
+    public class GameObjectEventUnit : AtomEventUnit<GameObject, GameObjectEventReference, GameObjectEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("GameObjectPair Event"), UnitTitle("Atom GameObjectPair Event")]
+    public class
+        GameObjectPairEventUnit : AtomEventUnit<GameObjectPair, GameObjectPairEventReference, GameObjectPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Int Event"), UnitTitle("Atom Int Event")]
+    public class IntEventUnit : AtomEventUnit<int, IntEventReference, IntEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("IntPair Event"), UnitTitle("Atom IntPair Event")]
+    public class IntPairEventUnit : AtomEventUnit<IntPair, IntPairEventReference, IntPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Quaternion Event"), UnitTitle("Atom Quaternion Event")]
+    public class QuaternionEventUnit : AtomEventUnit<Quaternion, QuaternionEventReference, QuaternionEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("QuaternionPair Event"), UnitTitle("Atom QuaternionPair Event")]
+    public class
+        QuaternionPairEventUnit : AtomEventUnit<QuaternionPair, QuaternionPairEventReference, QuaternionPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("String Event"), UnitTitle("Atom String Event")]
+    public class StringEventUnit : AtomEventUnit<string, StringEventReference, StringEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("StringPair Event"), UnitTitle("Atom StringPair Event")]
+    public class StringPairEventUnit : AtomEventUnit<StringPair, StringPairEventReference, StringPairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Vector2 Event"), UnitTitle("Atom Vector2 Event")]
+    public class Vector2EventUnit : AtomEventUnit<Vector2, Vector2EventReference, Vector2Event> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Vector2Pair Event"), UnitTitle("Atom Vector2Pair Event")]
+    public class Vector2PairEventUnit : AtomEventUnit<Vector2Pair, Vector2PairEventReference, Vector2PairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Vector3 Event"), UnitTitle("Atom Vector3 Event")]
+    public class Vector3EventUnit : AtomEventUnit<Vector3, Vector3EventReference, Vector3Event> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Vector3Pair Event"), UnitTitle("Atom Vector3Pair Event")]
+    public class Vector3PairEventUnit : AtomEventUnit<Vector3Pair, Vector3PairEventReference, Vector3PairEvent> { }
+
+    [UnitCategory("Events/Atoms"), UnitShortTitle("Void Event"), UnitTitle("Atom Void Event")]
+    public class VoidEventUnit : AtomEventUnit<Void, VoidBaseEventReference, VoidEvent> { }
 }
